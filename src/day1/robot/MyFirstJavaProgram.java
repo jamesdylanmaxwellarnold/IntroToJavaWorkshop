d21305038f629cc77c88c7898c1276ee727350bf
Ryan Keel <league@WTS11.attlocal.net>
2016-08-16 15:30:35 -0700

    dylan arnold day 2
    dy d 2

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -8,8 +8,33 @@ public class MyFirstJavaProgram {
 		
 		// START HERE
 
+		Robot jeff = new Robot ();	
+		
+jeff.penDown( );		
+jeff.move(100 );
+jeff.sparkle();	
+jeff.turn(90);	
+for (int i = 0; i <360; i++) {
 	
-		
-		
-	}
-}
+	
+	jeff.penDown();
+	jeff.move(1 );
+	jeff.sparkle();	
+	jeff.turn(360/360);	
+	jeff.setSpeed(10);
+	
+}	
+jeff.turn(90);	
+for (int i = 0; i <360; i++) {
+	
+	
+	jeff.penDown();
+	jeff.move(1 );
+	jeff.sparkle();	
+	jeff.turn(360/360);	
+	jeff.setSpeed(10);
+	jeff.turn(90)
+}		
+	
+	
+	}}
--- src/day1/robot/PentagonCrazy.java
+++ src/day1/robot/PentagonCrazy.java
@@ -10,26 +10,26 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-
+Robot robotnewrowbot = new Robot()  ;
 		// 3. Put the robot's pen down
-
+robotnewrowbot.penDown();
 		// 8. Make the robot go at maximum speed (10)
 
 		// 9. Set the pen to a color that you like for the shape
 
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
+int e= 360;
 		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
-
+int o=1;
 		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
 
 			// 2. Move the robot 200 pixels
-	
+	robotnewrowbot.move(200);
 			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
 	
 			// 6. Turn the robot the amount in your angle variable
 	
-			// 11. Turn the robot one more degree
+			//  11. Turn the robot one more degree
 
 	}
 
